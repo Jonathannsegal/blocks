7348d5d9d335845d53420e4646d79bd8faebcac3
jsegal <jsegal@iastate.edu>
2019-11-10 22:03:09 -0600

    fix problem with import

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -5,7 +5,7 @@ import Lottie from 'react-lottie'
 import * as locationdot from '../../db/locationdot.json'
 import ErrorScreen from '../../errors/ErrorScreen';
 import MapOverlay from './MapOverlay';
-import { RefreshTime } from '../../src/constants'
+import { RefreshTime } from '../../../src/constants'
 
 const locationdotOptions = {
 	loop: true,
