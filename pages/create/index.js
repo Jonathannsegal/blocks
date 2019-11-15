6478c16a8e68ff5dcd3bb56f339efa4fa8ac890b
jsegal <jsegal@iastate.edu>
2019-11-14 18:15:43 -0600

    update things

--- pages/create/index.js
+++ pages/create/index.js
@@ -5,8 +5,14 @@ import * as articulation from '../../src/db/articulation.json'
 import {
     Content,
     FlexboxGrid,
-    Button
+    Button,
+    Form,
+    FormGroup,
+    ControlLabel,
+    ButtonToolbar,
+    FormControl
 } from 'rsuite';
+import Map from '../../src/components/Create/MapExport';
 require('rsuite/lib/styles/index.less');
 
 const articulationOptions = {
@@ -22,27 +28,54 @@ const create = () => (
     <React.Fragment>
         <Content>
             <FlexboxGrid justify="center">
-                <FlexboxGrid.Item colspan={18}>
+                {/* <FlexboxGrid.Item colspan={18}>
                     <br /><br /><br />
                     <Lottie
                         options={articulationOptions}
                         isClickToPauseDisabled={true}
                     />
-                </FlexboxGrid.Item>
+                </FlexboxGrid.Item> */}
                 <FlexboxGrid.Item colspan={17}>
                     <br /><br />
                     <FlexboxGrid justify="space-around">
                         <FlexboxGrid.Item colspan={11}>
                             <Button size="lg" color="cyan" block href="/dashboard">Back</Button>
                         </FlexboxGrid.Item>
-                        <FlexboxGrid.Item colspan={11}>
+                        {/* <FlexboxGrid.Item colspan={11}>
                             <Button size="lg" color="cyan" block href="/game">Game</Button>
-                        </FlexboxGrid.Item>
+                        </FlexboxGrid.Item> */}
                     </FlexboxGrid>
                     <br />
                 </FlexboxGrid.Item>
             </FlexboxGrid>
+            <FlexboxGrid justify="center">
+                <FlexboxGrid.Item colspan={18}>
+                    <Form fluid>
+                        <FormGroup>
+                            <ControlLabel>Name</ControlLabel>
+                            <FormControl name="test" placeholder="Iowa State Campus" />
+                        </FormGroup>
+                        <div className="content2">
+                            <Map />
+                        </div>
+                        <br />
+                        <FormGroup>
+                            <FlexboxGrid justify="space-around">
+                                <FlexboxGrid.Item colspan={11}>
+                                    <Button size="lg" appearance="primary" color="cyan" type="submit">Make Game</Button>
+                                </FlexboxGrid.Item>
+                            </FlexboxGrid>
+                        </FormGroup>
+                    </Form>
+                </FlexboxGrid.Item>
+            </FlexboxGrid>
         </Content>
+        <style jsx>{`
+                    .content2 {
+                        height: 30vh;
+                        width: 100%;
+                    }
+				`}</style>
     </React.Fragment >
 );
 
---
+++ src/components/Create/Map.js
@@ -0,0 +1,156 @@
+import { Component } from 'react';
+import ReactMapGL, { FullscreenControl } from 'react-map-gl';
+import { geolocated } from 'react-geolocated';
+import Lottie from 'react-lottie'
+import * as pencil from '../../db/pencil.json'
+import * as trash from '../../db/trash.json'
+import * as resize from '../../db/resize.json'
+import ErrorScreen from '../../errors/ErrorScreen';
+import { RefreshTime } from '../../../src/constants'
+import { Editor, EditorModes } from 'react-map-gl-draw';
+import { getFeatureStyle, getEditHandleStyle } from './style';
+
+const pencilOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: pencil.default,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
+const trashOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: trash.default,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
+const resizeOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: resize.default,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
+class Map extends Component {
+    state = {
+        viewport: {
+            width: '100%',
+            height: '100%',
+            latitude: 42.03,
+            longitude: -93.645,
+            zoom: 14
+        },
+        mode: EditorModes.READ_ONLY
+    };
+
+    _onSelect = options => {
+        this.setState({ selectedFeatureIndex: options && options.selectedFeatureIndex });
+    };
+
+    _onUpdate = ({ editType }) => {
+        if (editType === 'addFeature') {
+            this.setState({
+                mode: EditorModes.EDITING
+            });
+        }
+    };
+
+    _onDelete = () => {
+        const selectedIndex = this.state.selectedFeatureIndex;
+        if (selectedIndex !== null && selectedIndex >= 0) {
+            this._editorRef.deleteFeatures(selectedIndex);
+        }
+    };
+
+    _renderDrawTools = () => {
+        return (
+            <React.Fragment>
+                <div className="mapboxgl-ctrl-top-left">
+                    <div className="mapboxgl-ctrl-group mapboxgl-ctrl">
+                        <button
+                            className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_polygon"
+                            title="Polygon tool (p)"
+                            onClick={() => this.setState({ mode: EditorModes.DRAW_POLYGON })}
+                        > <Lottie
+                                height={28}
+                                width={28}
+                                options={pencilOptions}
+                                isClickToPauseDisabled={true}
+                            /> </button>
+                        <button
+                            className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_trash"
+                            title="Delete"
+                            onClick={this._onDelete}
+                        ><Lottie
+                                height={22}
+                                width={22}
+                                options={trashOptions}
+                                isClickToPauseDisabled={true}
+                            /></button>
+                    </div>
+                </div>
+                <div className="mapboxgl-ctrl-top-right">
+                    <div className="mapboxgl-ctrl-group mapboxgl-ctrl">
+                        <button
+                            className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_polygon"
+                            title="Polygon tool (p)"
+                            onClick={() => this.setState({ mode: EditorModes.DRAW_POLYGON })}
+                        > <Lottie
+                                height={28}
+                                width={28}
+                                options={resizeOptions}
+                                isClickToPauseDisabled={true}
+                            /> </button>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    };
+
+    render() {
+        const { mode } = this.state;
+        return !this.props.isGeolocationAvailable ? (
+            <ErrorScreen message={`Your browser \n does not support \n Geolocation`} />
+        ) : !this.props.isGeolocationEnabled ? (
+            <ErrorScreen message={`Geolocation \n is not \n enabled \n\n Big Sad`} />
+        ) : this.props.coords ? (
+            <React.Fragment>
+                <ReactMapGL
+                    mapStyle="mapbox://styles/mapbox/streets-v9"
+                    mapboxApiAccessToken="pk.eyJ1Ijoiam9uYXRoYW5zZWdhbCIsImEiOiJjamxrODVuamgwazI0M3BsZHIwNW5xZjNrIn0.UTtfn21uo6LCNkh-Pn1b4A"
+                    {...this.state.viewport}
+                    onViewportChange={(viewport) => this.setState({ viewport })}
+                >
+                    <Editor
+                        ref={_ => (this._editorRef = _)}
+                        style={{ width: '100%', height: '100%' }}
+                        clickRadius={12}
+                        mode={mode}
+                        onSelect={this._onSelect}
+                        onUpdate={this._onUpdate}
+                        editHandleShape={'circle'}
+                        featureStyle={getFeatureStyle}
+                        editHandleStyle={getEditHandleStyle}
+                    />
+                    {this._renderDrawTools()}
+                </ReactMapGL>
+            </React.Fragment>
+        ) : (
+                        <div>Getting the location data&hellip; </div>
+                    );
+    }
+}
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    userDecisionTimeout: RefreshTime.fiveSeconds,
+    watchPosition: true
+})(Map);
